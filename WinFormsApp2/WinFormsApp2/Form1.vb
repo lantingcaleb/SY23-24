259e26fac7826e639ffa4bd0e596d4ce51cae687
Caleb <lantingcaleb@gmail.com>
2024-01-08 12:52:54 -0700

    1/8/24 New Project

---
+++ WinFormsApp2/WinFormsApp2/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub MenuStrip1_ItemClicked(sender As Object, e As ToolStripItemClickedEventArgs) Handles MenuStrip1.ItemClicked
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+
+    End Sub
+End Class
